4c0c57edbe99b0ea37e844f3e05e0aafe0f4f4fd
Michel <contato@virandodev.com>
2025-02-13 12:18:43 -0300

    MODULO-02-AULA-05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
