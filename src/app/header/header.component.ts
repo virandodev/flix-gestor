2c23feee6f5f7d39469113330397fa406e544fb7
Michel <contato@virandodev.com>
2025-03-03 09:27:58 -0300

    MODULO-02-AULA-26

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  readonly dialog = inject(MatDialog);
+
   onLogin() {
-    window.alert("Logando...");
+    this.dialog.open(LoginComponent);
   }
 }
