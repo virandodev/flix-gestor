c5c424cb8184bd1e2d31bd53632a76210d9907ab
Michel <contato@virandodev.com>
2025-03-03 10:11:51 -0300

    MODULO-02-AULA-28

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   usuario = '';
   senha = '';
+  nome = '';
+  nascimento = '';
+  isCadastrar = false;
 
   test() {
     console.log('Meu usuario:', this.usuario, ' possui a senha: ', this.senha);
+  }
+
+  onCadastrar() {
+    this.isCadastrar = !this.isCadastrar;
   }
 
 }
