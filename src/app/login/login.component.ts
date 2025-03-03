fcb5f5091bd2dc389ce060e92704d2bde21da042
Michel <contato@virandodev.com>
2025-03-03 09:44:26 -0300

    MODULO-02-AULA-27

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  usuario = '';
+  senha = '';
+
+  test() {
+    console.log('Meu usuario:', this.usuario, ' possui a senha: ', this.senha);
+  }
 
 }
